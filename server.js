288cb392bd162c6a02951d2427a11344ef41e38d
imanisul <ianisul768@gmail.com>
2026-06-28 13:26:35 +0530

    rabbitmq is added in server.js

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ require("dotenv").config();
 const app = require("./src/app.js");
 const connectDB = require("./src/config/db.js");
 
+const {
+    connectRabbitMQ
+} = require('./src/config/rabbitmq.js');
+
 const PORT = process.env.PORT;
 
 
 connectDB();
+
+connectRabbitMQ();
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
