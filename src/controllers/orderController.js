83695d86b493d7a35ed1fc1fe1faaf45a11f90b1
imanisul <ianisul768@gmail.com>
2026-06-28 11:59:33 +0530

    CRUD operation created

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,8 +1,12 @@
 const Order = require('../models/order.js');
 
+
+
+//create order
+
 const createOrder = async (req, res) => {
     try{
-        const order = await Order.create(req,body);
+        const order = await Order.create(req.body);
         res.status(201).json(order);
     }
     catch(error){
@@ -10,6 +14,83 @@ const createOrder = async (req, res) => {
     }
 };
 
+// get all orders 
+
+const getAllOrders = async (req, res) => {
+    try{
+        const orders = await Order.find();
+        res.status(200).json(orders);
+    }
+    catch(error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+//get order by id 
+
+const getOrderById = async (req, res) => {
+    try{
+        const order = await Order.findById(req.params.id);
+
+        if(!order){
+            return res.status(404).json({
+                message: "Order not found"
+            });
+        }
+        res.status(200).json(order);
+    }
+    catch(error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+
+//update order 
+
+const updateOrder = async (req, res) => {
+    try{
+        const order = await Order.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true}
+        );
+
+        if(!order){
+            return res.status(404).json({
+                message: "Order not found"
+            });
+        }
+        res.status(200).json(order);
+    }
+    catch(error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+// Delete Order
+
+const deleteOrder = async (req, res) => {
+    try{
+        const order = await Order.findByIdAndDelete(req.params.id);
+
+        if(!order){
+            return res.status(404).json({
+                message: "Order not found"
+            });
+        }
+        res.status(200).json({
+            message: "Order deleted successfully"
+        });
+    }
+    catch(error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
 module.exports = {
-    createOrder
+    createOrder,
+    getAllOrders,
+    getOrderById,
+    updateOrder,
+    deleteOrder    
 };
